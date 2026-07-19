ef7166262f74389ae79380c09aed4f294dd71f88
sthakeshar <86221217+sthakeshar@users.noreply.github.com>
2026-07-19 20:50:17 +0900

    Time: 0 ms (100%), Space: 12.9 MB (65.13%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,20 +1,9 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        n_len=len(nums)
-        if nums[0]>target:
-            return 0
-
-        if nums[n_len-1]<target:
-            return n_len
-            
-        for i in range(n_len):
-            if nums[i]==target:
+        for i in range(len(nums)):
+            # If we find the target, or the first number GREATER than target
+            if nums[i] >= target:
                 return i
-                break
-            elif nums[i]>target:
-                return i
+        
+        # If we finish the loop, it means target is larger than everything
+        return len(nums)
